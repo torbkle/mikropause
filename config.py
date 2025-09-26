5fc15bbcf7721fb516c80afc4fffba858a03ca60
torbkle <torkle05553@stud.noroff.no>
2025-09-26 12:10:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+ikon_urls = {
+    "Pust": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/icons/wind.svg",
+    "Skjermpause": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/icons/eye-off.svg",
+    "Fokus": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/icons/target.svg",
+    "Bevegelse": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/icons/stretch-horizontal.svg"
+}
+
 lyd_urls = {
     "Pust": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/audio/pust.mp3",
     "Skjermpause": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/audio/skjermpause.mp3",

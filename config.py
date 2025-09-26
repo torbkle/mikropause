6bee3b768a45454add3df5cdd321d36db5535696
torbkle <torkle05553@stud.noroff.no>
2025-09-26 12:00:08 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+lyd_urls = {
+    "Pust": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/audio/pust.mp3",
+    "Skjermpause": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/audio/skjermpause.mp3",
+    "Fokus": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/audio/fokus.mp3",
+    "Bevegelse": "https://raw.githubusercontent.com/torbkle/mikropause/main/assets/audio/bevegelse.mp3"
+}
